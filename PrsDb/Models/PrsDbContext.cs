91733988ffd7a9fd18813419779a07f44ad96a91
Billy Dotson <billybad5@gmail.com>
2022-09-22 11:20:29 -0400

    Add project files.

---
+++ PrsDb/Models/PrsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PrsDb.Models {
+
+    public class PrsDbContext : DbContext {
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<Requestline> Requestlines { get; set; }
+
+        public PrsDbContext(DbContextOptions<PrsDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+
+    }
+}
